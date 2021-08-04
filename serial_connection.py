824d759dfa43f569716318c777fa8b081b6be39c
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-08-04 12:44:56 +0100

    Created a script that takes command line arguments and sends a serial message

---
+++ serial_connection.py
@@ -0,0 +1,8 @@
+import serial
+import sys
+from time import sleep
+
+ser = serial.Serial(sys.argv[1], sys.argv[2], timeout = 1)
+sleep(3)
+ser.write(bytes(sys.argv[3] + "\r", "utf-8"))
+print(ser.read(10))
