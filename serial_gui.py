d440741d604599270adf474b0a04fc22b64a34e9
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-09-01 12:41:28 +0100

    EVERYTHING FROM SESSION 4. Creating an application class to organise everything.

--- serial_gui.py
+++ serial_gui.py
@@ -1,24 +1,36 @@
 import tkinter
-
-#from serial_connection import write_to_serial
 from mock_arduino import mock_arduino_response
 
-main_window = tkinter.Tk()
 
-def send_button_click():
-    output_box.configure(text=mock_arduino_response(input_box.get("1.0", 'end-1c')))
-    print(input_box.get("1.0", 'end-1c'))
+class Application(tkinter.Frame):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        self.main_window = main_window
+        self.create_widgets()
 
-my_label = tkinter.Label(text="Put a message here please :)")
-my_label.grid(row=0,column=0)
+    def send_button_click(self, event = None):
+        print(event)
+        self.output_box.configure(text = mock_arduino_response(self.input_box.get()))
 
-input_box = tkinter.Text(height=1, width=30)
-input_box.grid(row=1)
+    def create_widgets(self):
+        self.my_label = tkinter.Label(text = "Put a message here please :)")
+        self.my_label.grid(row = 0,column = 0)
 
-serial_connection_button = tkinter.Button(main_window, text = "Send", command = send_button_click)
-serial_connection_button.grid(row=2, column=0)
+        self.input_box = tkinter.Entry(width = 30)
+        self.input_box.grid(row = 1)
+        self.input_box.bind("<Return>", self.send_button_click)
 
-output_box = tkinter.Label(text="This is the output message box")
-output_box.grid(row=4)
+        self.serial_connection_button = tkinter.Button(self.main_window, text = "Send", command = self.send_button_click)
+        self.serial_connection_button.grid(row = 2, column = 0)
 
-main_window.mainloop()
+        self.output_box = tkinter.Label(text = "This is the output message box")
+        self.output_box.grid(row = 4)     
+    
+def main():
+    main_window = tkinter.Tk()
+    gui = Application(main_window)
+    main_window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
